ae62086ca6b303688cc7a9c78fa1ae9ac94604bb
Julius Polar <julius.polar@uniscon.com>
2024-06-03 14:31:34 +0200

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src/**/*.{ts,tsx}", "./index.html"],
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./index.html",
+    "./node_modules/@julianelda/scratchpad/dist/scratchpad.js",
+  ],
   theme: {
     extend: {},
     fontFamily: {
