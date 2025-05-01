7196c8babda0391d021cec6db30193e625c8aa4e
Julius Polar <julius.polar@protonmail.com>
2025-05-01 09:15:03 +0200

    updated meter url

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
         <p className="pt-4">
           <Hyperlink
             title="meter"
-            href="https://meter-nine.vercel.app/"
+            href="https://julianelda.io/meter/"
           />
         </p>
         <p>
